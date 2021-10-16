b0d758ace0fb375748c45c87f89f6278cf52d442
Tolga Secme <tolgasecme@icloud.com>
2021-10-16 12:04:30 -0600

    initials

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Conventional props
+function Heading({ title }: { title: string }) {
+  return (
+    <h1>{title}</h1>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="TypeScript Training"></Heading>
     </div>
   );
 }
